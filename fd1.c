584aca0604e52fea5157aaa10f396f0a1e8cec4b
chaerin0514 <etqw3512@gmail.com>
2025-04-09 10:26:42 +0900

    commit

---
+++ fd1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void main()
+{
+	int fd;
+	fd = open("a.txt", O_CREAT | O_RDONLY);
+	if (fd < 0) {
+		perror("Could not open a.txt");
+		exit(1);
+	} else {
+		printf("open success");
+		printf("file discriptor: %d\n", fd);
+	}
+	close(fd);
+}
